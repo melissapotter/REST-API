ea527de1fab294a64e2c2622694da25adb4e22be
Melissa <melissapotter.student@careerdevs.com>
2019-01-06 23:29:56 +0000

    .get, function (req,res), res.send, ssetting up express app

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express')
 
 const app = express();
 
+app.get('/api', function(req,res){
+    console.log('GET request');
+    res.send({ name: 'Meli' });
+});
+
 //listen for requests
 app.listen(process.env.port || 4000, function(){
    console.log('now listening for requests'); 
