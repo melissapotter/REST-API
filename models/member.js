6ac82ec68e6473c9948ce8f4ccec9115a031d5e3
Melissa <melissapotter.student@careerdevs.com>
2019-01-09 00:15:48 +0000

    geoSchema, geomtery, GeoJSON

--- models/member.js
+++ models/member.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
+//create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    
+});
+
 //create member Schema and model
 const MemberSchema = new Schema({
     name: {
@@ -14,8 +29,8 @@ const MemberSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
-    // add in geo location 
+    },
+    // geometry: GeoSchema
 });
 
 const Member = mongoose.model('member', MemberSchema);
