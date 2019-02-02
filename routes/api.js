945d8b16aa2997acf2d0af81a4fdf2fbf19e3f07
Melissa <melissapotter.student@careerdevs.com>
2019-02-02 19:35:24 +0000

    working file

--- routes/api.js
+++ routes/api.js
@@ -1,46 +1,52 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Member = require('../models/member.js');
+const Member = require('../models/member');
 
-// get a list of member from the database
-router.get('/members', function (req,res,next){
-    // Member.find({}).then(function(members){
-    //     res.send(members);
-    // }); - return all members
-    Member.geoNear(
-        {type: "Point", coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
-        {maxDistance: 100000, spherical: true}
-        ).then(function(members){
-            res.send(members);
-        });
-    
+// GET request
+router.get("/members", function(req, res, next) {
+  Member.aggregate([{
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
+      distanceField: "dist.calculated",
+      includeLocs: "dist.location",
+      maxDistance: 100000,
+      spherical: true
+    }
+  }])
+    .then(function(member) {
+      res.send(member);
+    })
+    .catch(next);
 });
 
-// add a new member to the database
-router.post('/members', function (req,res,next){
-    // var member = new Member(req.body);
-    // member.save();
-    Member.create(req.body).then(function(member){
+
+// POST request | add a new member to the db
+router.post('/members', function(req, res, next) {
+    Member.create(req.body).then(function(member) {
         res.send(member);
     }).catch(next);
 });
-    
 
-// update a member in the datbase
-router.put('/members/:id', function (req,res,next){
-    Member.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
-        Member.findOne({_id: req.params.id}).then(function(member){
+// UPDATE (put) request to put a new record in the db/collection
+router.put('/members/:id', function(req, res, next) {
+    Member.findByIdAndUpdate({_id:req.params.id}, req.body).then(function() {
+        Member.findOne({_id:req.params.id}).then(function(member) {
            res.send(member);  
         });
-       
+        
+    });
+   
+});
+
+// DELETE request
+router.delete('/members/:id', function(req, res, next) {
+    Member.findByIdAndRemove({_id:req.params.id}).then(function(member) {
+        res.send(member);
     });
 });
 
-// delete a member from the datatbase
-router.delete('/members/:id', function (req,res,next){
-    Member.findByIdAndRemove({_id: req.params.id}).then(function(member){
-    res.send(member);
-    });
-});
 
 module.exports = router;
